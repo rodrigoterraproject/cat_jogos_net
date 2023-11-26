90dfaae93e26fecff3cd3b47ba8dbeb01b0c30ee
rodrigoterraproject <rodrigoterradigital@gmail.com>
2023-11-25 22:20:10 -0300

    first

---
+++ ApiCatalogoJogos/Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+
+        public JogoJaCadastradoException()
+            : base("Este jogo Já está cadastrado")
+        {
+
+        }
+    }
+}
